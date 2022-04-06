8a94b5f31f20175011461af3b90724153146e6d9
Kalpesh <44432005+calpesh@users.noreply.github.com>
2022-04-06 07:06:02 +0000

    update 1

--- .ccls-cache/@home@runner@AttentiveFortunateInvocation/main.cpp
+++ .ccls-cache/@home@runner@AttentiveFortunateInvocation/main.cpp
@@ -4,13 +4,14 @@ int main() {
   int a = 5;
   int *p;
   p = &a;
-  std::cout << a << " " << &a << "\n";
-  std::cout << p << " " << &p << "\n";
-  std::cout << *p << "\n";
+  std::cout << a << " " << &a << "\n";//val of a, address of a(abc)
+  std::cout << p << " " << &p << "\n";//val of p(address of a(abc)),address of p(xyz)
+  std::cout << *p << "\n";//val at address stored in p(val of a)
   int b = 20;
-  p = &b;
-  std::cout << b << " " << &b << "\n";
-  std::cout << p << " " << &p << "\n";
-  std::cout << *p << "\n";
-  std::cout << a << " " << &a << "\n";
+  *p = b;//update value at a
+  //p=&b update address of p and point to b now instead a
+  std::cout << b << " " << &b << "\n";//val of b, address of b
+  std::cout << p << " " << &p << "\n";//val of p(address of a),address of p
+  std::cout << *p << "\n";//val at address stored in p
+  std::cout << a << " " << &a << "\n";//val of a, address of a
 }
